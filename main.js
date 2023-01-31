ed6dc0d6c011645a7530ab17690507075c1b1d81
shauryal21 <82619067+shauryal21@users.noreply.github.com>
2023-01-31 18:37:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas')
+	
+	instializeInSetup(mario);
+
+	video= createCapture(VIDEO);
+	video.size(800,300);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+	console.log('JEEZ, MODELOADED, YOU DONT HAVE TO KEEP LOOKING. THIS IS NOT SOME TOP SECRET FBI STUFF, THIS IS JUST SOME NERDY INDIAN KIDS WEBSITE ')
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+
+	}
+}
+function draw() {
+	game()
+}
+
+
+
+
+
+
